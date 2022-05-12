e32a9dc512de735f6a427dba860f436bc5dcf1be
Omer Koskas <omerk96@gmail.com>
2022-05-12 18:52:56 +0300

    Added Conveyor Belt Logic

---
+++ conveyor.py
@@ -0,0 +1,24 @@
+import pybullet as p
+
+
+class Conveyor(object):
+    def __init__(self, location, speed=5, urdf_path=None):
+        if not urdf_path:
+            urdf_path = 'models/conveyor/conveyor.urdf'
+
+        start_orientation = p.getQuaternionFromEuler([0, 0, 0])
+        self.id = p.loadURDF(urdf_path, location, start_orientation, useFixedBase=True)
+        self.speed = speed
+
+    def convey(self):
+        contact_points = p.getContactPoints(bodyA=self.id)
+        # 2: bodyUniqueIdB
+        # 4: linkIndexB
+        # 6: positionOnB
+        links = [(point[2], point[4], point[6]) for point in contact_points]
+
+        for body_uid, _, _ in links:
+            linear_velocity, angular_velocity = p.getBaseVelocity(body_uid)
+            vx, _, vz = linear_velocity
+            linear_velocity = (vx, self.speed, vz)
+            p.resetBaseVelocity(body_uid, linear_velocity, angular_velocity)
--- simulation.py
+++ simulation.py
@@ -2,34 +2,47 @@ import os
 import time
 import pybullet as p
 import pybullet_data
-# from Ur5 import Ur5
 from multiarm_planner.UR5 import UR5
 
 URDF_FILES_PATH = "models"
 
+CONVEYOR_LOCATION = [0, 0, 0.5]
 BINS_LOCATIONS = [[1.5, 0.0, 0.1], [-1.5, 0.0, 0.1]]
 UR5_LOCATIONS = [([1, 0, 1], [0, 0, 1, 0]), ([-1, 0, 1], [0, 0, 1, 0])]
+FRAME_RATE = 1 / 240.
 
 
-# Open GUI and set pybullet_data in the path
-p.connect(p.GUI)
-# p.resetDebugVisualizerCamera(3, 90, -30, [0.0, -0.0, -0.0])
-p.setTimeStep(1 / 240.)
+def create_conveyor():
+    start_orientation = p.getQuaternionFromEuler([0, 0, 0])
+    return p.loadURDF('models/conveyor/conveyor.urdf', CONVEYOR_LOCATION, start_orientation, useFixedBase=True)
 
-# Load plane contained in pybullet_data
-plane_id = p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"))
 
-bins_ids = [p.loadURDF(os.path.join(URDF_FILES_PATH, "bin.urdf"), bin_loc, flags=p.URDF_USE_INERTIA_FROM_FILE,
-                   useFixedBase=True) for bin_loc in BINS_LOCATIONS]
+def create_arms():
+    return [UR5(ur5_loc) for ur5_loc in UR5_LOCATIONS]
 
-ur5_arms = [UR5(ur5_loc) for ur5_loc in UR5_LOCATIONS]
 
-trash_id = p.loadURDF(os.path.join(URDF_FILES_PATH, 'YcbMustardBottle', "model.urdf"), [0., 0., 0.],
-                      flags=p.URDF_USE_INERTIA_FROM_FILE)
+def create_bins():
+    return [p.loadURDF(os.path.join(URDF_FILES_PATH, "bin.urdf"), bin_loc, flags=p.URDF_USE_INERTIA_FROM_FILE,
+                       useFixedBase=True) for bin_loc in BINS_LOCATIONS]
 
-p.setGravity(0, 0, -9.8)
+
+def create_env():
+    ids = {}
+    p.connect(p.GUI)
+    p.setGravity(0, 0, -9.8)
+
+    ids['plane'] = p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"))
+    ids['bins'] = create_bins()
+    ids['arms'] = create_arms()
+    ids['conveyor'] = create_conveyor()
+
+    return ids
 
 
 if __name__ == '__main__':
-    ur5_arms[0].end_effector.close()
-    ur5_arms[0].end_effector.open()
+    create_env()
+    for _ in range(10000):
+        p.stepSimulation()
+        time.sleep(FRAME_RATE)
+    # ur5_arms[0].end_effector.close()
+    # ur5_arms[0].end_effector.open()
