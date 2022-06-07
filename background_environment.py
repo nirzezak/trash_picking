7f947f9a9f0c8bc8a62d756468228a14cbd38cff
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2022-06-07 21:28:58 +0300

    Enhanced compute_motion_plan structure

--- background_environment.py
+++ background_environment.py
@@ -11,21 +11,38 @@ class BackgroundEnv(Environment):
         """
         super().__init__(connection_mode, conveyor_speed=0, set_pybullet_utils_p=True)
 
-    def compute_motion_plan(self, arm_index_to_trash: dict, start_configs=None):
+    def compute_motion_plan(self, arms_idx, trash, bin_locations, start_configs):
         """"
-        @param arm_index_to_trash: dictionary that maps from arm index (from self.arms)
-        to a trash: (TrashConfig, trash location)
+        @param arms_idx: arm indices to find paths for
+        @param trash: a tuple: (TrashConfig, trash location), same order as in arms_idx
+        @param bin_locations: list of bin locations, same order as in arms_idx
+        @param start_configs: list of start configs of the arms, same order as in arms_idx
 
-        @param start_configs: list of start configs of the arms (same order as in arm_index_to_trash TODO- this is ugly)
+        @returns: paths to trash, paths to bin
+        paths to trash - a list of paths to appropriate trash, same order as in arms_idx
+        paths to bins - a list of paths to appropriate bins, same order as in arms_idx
 
-        @returns a dictionary that maps from arms indices to paths, each arm gets the pass that will lead it to
-        its trash target
+        TODO - Right now the function assumes that we run this with only one arm
         """
+        paths_to_trash = self.compute_path_to_trash(arms_idx, trash, start_configs)
+        last_configs = [paths_to_trash[-1]]  # TODO - change this when we will work with multi arms
+        paths_to_bins = self.compute_path_to_bin(arms_idx, bin_locations, last_configs)
 
+        return paths_to_trash, paths_to_bins
+
+    def compute_path_to_trash(self, arms_idx, trash, start_configs):
+        """"
+        @param arms_idx: arm indices to find paths for
+        @param trash: a tuple: (TrashConfig, trash location), same order as in arms_idx
+        @param start_configs: list of start configs of the arms, same order as in arms_idx
+
+        @returns a list of paths to appropriate trash, same order as in arms_idx
+        TODO - Right now the function assumes that we run this with only one arm
+        """
         arms_to_above_position_configs = {}
         arms_to_actual_goal_configs = {}
 
-        for arm_idx, trash_info in arm_index_to_trash.items():
+        for arm_idx, trash_info in zip(arms_idx, trash):
             # TODO: Fix this entire block of code because it is super specific
             trash = self.trash_generator.summon_trash(trash_config=trash_info[0], forced_location=trash_info[1])
             trash_pos, orientation = self.p_simulation.getBasePositionAndOrientation(trash.get_id())
@@ -53,17 +70,18 @@ class BackgroundEnv(Environment):
 
         return path
 
-    def path_to_bin(self, arms_idx, bin_locations, start_configs):
+    def compute_path_to_bin(self, arms_idx, bin_locations, start_configs):
         """"
         @param arms_idx: arm indices to find paths for
-        @param bin_locations: list of bin locations (same order as in arm indices)
-        @param start_configs: list of start configs of the arms (same order as in arm_index_to_trash TODO- this is ugly)
+        @param bin_locations: list of bin locations, same order as in arms_idx
+        @param start_configs: list of start configs of the arms, same order as in arms_idx
 
-        @returns a dictionary that maps from arms indices to paths, each arm gets the path that will lead it to
-        the appropriate bin
+        @returns a list of paths to appropriate bins, same order as in arms_idx
+        TODO - Right now the function assumes that we run this with only one arm
         """
-
-        bin_location = list(bin_locations)
-        bin_location[2] += 0.85
-        end_pos = [bin_location, p.getQuaternionFromEuler([0, np.pi / 2, np.pi / 2])]
-        return self.arms_manager.birrt([self.arms[arm_idx] for arm_idx in arms_idx], [end_pos], start_configs)
+        end_poses = []
+        for bin_loc in bin_locations:
+            bin_loc = list(bin_loc)
+            bin_loc[2] += 0.85
+            end_poses.append([bin_loc, p.getQuaternionFromEuler([0, np.pi / 2, np.pi / 2])])
+        return self.arms_manager.birrt([self.arms[arm_idx] for arm_idx in arms_idx], end_poses, start_configs)
