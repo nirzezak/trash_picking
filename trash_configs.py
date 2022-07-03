cd8f0b271db59d805be1dbbf1141b3b9c7baa7c4
OmerK96 <omerk96@gmail.com>
2022-07-03 11:26:12 +0300

    Updated mustard spawn location
    I have updated the spawn location of mustard, so it would be in the
    start of the new, bigger, conveyor belt, rather than at the center.

--- trash_configs.py
+++ trash_configs.py
@@ -5,6 +5,6 @@ class TrashConfig(Enum):
 
     MUSTARD = {
         'path': r'models/YcbMustardBottle/model.urdf',
-        'location': [0, 0, 0.7],
+        'location': [0, -2.5, 0.57],
         'gripping_points': [[0, 0, 1]],
     }
