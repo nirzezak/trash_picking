c088150f0dd4386b8986e6b33ceff0bca342cd62
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2022-07-24 17:43:26 +0300

    Revert "Revert "Task manager small improvements""
    This reverts commit 73b84fb6

--- score.py
+++ score.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 import pybullet as p
 
 
 class Score(object):
-    def __init__(self, location=None):
+    def __init__(self, location: Optional[list[int]] = None):
         if not location:
             location = [0, 0, 2]
         self.location = location
@@ -12,6 +14,7 @@ class Score(object):
         self.id = p.addUserDebugText(msg, self.location, textColorRGB=self.color)
 
     def increase_score(self):
+        p.removeUserDebugItem(self.id)
         self.score += 1
         msg = f'score = {self.score}'
         self.id = p.addUserDebugText(msg, self.location, textColorRGB=self.color,
