c1de526be9dfa3f1b5ce633f3d6dda05009d9118
OmerK96 <omerk96@gmail.com>
2022-08-06 14:36:49 +0300

    Made the trash spawn in a nicer location
    * This will make the arms have an easier time picking up the trash (only for the arms away from the camera, thou)
    * In the future, the spawn location would be dynamic anyways, so it is more of a convenience commit...

--- configs/trash_configs.py
+++ configs/trash_configs.py
@@ -6,19 +6,19 @@ class TrashConfig(Enum):
 
     MUSTARD = {
         'path': r'models/YcbMustardBottle/model.urdf',
-        'location': [0, -2.5, 0.57],
+        'location': [-0.15, -2.5, 0.57],
         'gripping_points': [[0, 0, 1]],
         'trash_type': trash_types.TrashTypes.PLASTIC
     }
     METAL_CAN = {
         'path': r'models/YcbMasterChefCan/model.urdf',
-        'location': [0, -2.5, 0.57],
+        'location': [-0.15, -2.5, 0.57],
         'gripping_points': [[0, 0, 1]],
         'trash_type': trash_types.TrashTypes.ELECTRONIC
     }
     PAPER_BOX = {
         'path': r'models/YcbCrackerBox/model.urdf',
-        'location': [0, -2.5, 0.57],
+        'location': [-0.15, -2.5, 0.57],
         'gripping_points': [[0, 0, 1]],
         'trash_type': trash_types.TrashTypes.PAPER
     }
