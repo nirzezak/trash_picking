e7b2be66cbc0f66969e9db6c8f43f5fe0e0d79a9
nirzezak <nirzezak@mail.tau.ac.il>
2022-08-09 13:59:47 +0300

    Improving spawn locations and gripping points of all current trash objects

--- configs/trash_configs.py
+++ configs/trash_configs.py
@@ -6,19 +6,19 @@ class TrashConfig(Enum):
 
     MUSTARD = {
         'path': r'models/YcbMustardBottle/model.urdf',
-        'location': [-0.15, -2.5, 0.57],
-        'gripping_points': [[0, 0, 1]],
+        'location': [-0.2, -2.5, 0.57],
+        'gripping_points': [[0.2, 0.11, 0.13]],
         'trash_type': trash_types.TrashTypes.PLASTIC
     }
     METAL_CAN = {
         'path': r'models/YcbMasterChefCan/model.urdf',
-        'location': [-0.15, -2.5, 0.57],
-        'gripping_points': [[-0.044, -0.35, -0.01]],
+        'location': [-0.2, -2.5, 0.57],
+        'gripping_points': [[0.2, 0.11, 0]],
         'trash_type': trash_types.TrashTypes.ELECTRONIC
     }
     PAPER_BOX = {
         'path': r'models/YcbCrackerBox/model.urdf',
         'location': [-0.15, -2.5, 0.57],
-        'gripping_points': [[0, 0, 1]],
+        'gripping_points': [[0.2, 0.11, 0.175]],
         'trash_type': trash_types.TrashTypes.PAPER
     }
