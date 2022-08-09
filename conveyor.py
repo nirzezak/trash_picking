e7211e502239c5e8c3c1b8ec15b05be5105bca6c
nirzezak <nirzezak@mail.tau.ac.il>
2022-08-09 13:58:58 +0300

    Stopping the conveying of trash while it is being picked up

--- conveyor.py
+++ conveyor.py
@@ -22,10 +22,10 @@ class Conveyor(object):
         self.p_simulation = p_simulation
         self.id = p_simulation.loadURDF(urdf_path, location, start_orientation, useFixedBase=True)
         self.speed = speed
-        self.arms_ids = []
+        self.dont_convey = []
         if arms:
-            self.arms_ids = [arm.body_id for arm in arms]
-            self.arms_ids += [arm.end_effector.body_id for arm in arms]
+            self.dont_convey = [arm.body_id for arm in arms]
+            self.dont_convey += [arm.end_effector.body_id for arm in arms]
 
     def convey(self):
         contact_points = self.p_simulation.getContactPoints(bodyA=self.id)
@@ -35,10 +35,16 @@ class Conveyor(object):
         links = [(point[2], point[4], point[6]) for point in contact_points]
 
         for body_uid, link_index, _ in links:
-            if body_uid in self.arms_ids:
+            if body_uid in self.dont_convey:
                 continue
             linear_velocity, _ = self.p_simulation.getBaseVelocity(body_uid)
             _, _, vz = linear_velocity
             linear_velocity = (0, self.speed, vz)
             self.p_simulation.resetBaseVelocity(body_uid, linear_velocity, (0, 0, 0))
-            self.p_simulation.changeDynamics(body_uid, link_index, lateralFriction=0, anisotropicFriction=0)
+            self.p_simulation.changeDynamics(body_uid, link_index, lateralFriction=0)
+
+    def unconvey(self, uid):
+        # TODO: Can new trash have the same ID as old removed trash?
+        #   Either way, should probably remove the ID from this list when removing trash for less spam
+        if uid not in self.dont_convey:
+            self.dont_convey.append(uid)
