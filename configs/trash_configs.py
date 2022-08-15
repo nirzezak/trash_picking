65420000cd11f9202a7ba4c21d7d844c3084cca3
nirzezak <nirzezak@mail.tau.ac.il>
2022-08-15 20:19:52 +0300

    Bugfixing gripping points of trash

--- configs/trash_configs.py
+++ configs/trash_configs.py
@@ -6,19 +6,19 @@ class TrashConfig(Enum):
 
     MUSTARD = {
         'path': r'models/YcbMustardBottle/model.urdf',
-        'location': [-0.2, -2.5, 0.57],
-        'gripping_points': [[0.2, 0.11, 0.13]],
+        'location': [-0.2, -2.5, 0.61],
+        'gripping_points': [[0.02, 0.05, 0.175]],
         'trash_type': trash_types.TrashTypes.PLASTIC
     }
     METAL_CAN = {
         'path': r'models/YcbMasterChefCan/model.urdf',
-        'location': [-0.2, -2.5, 0.57],
-        'gripping_points': [[0.2, 0.11, 0]],
+        'location': [-0.2, -2.5, 0.61],
+        'gripping_points': [[0.02, 0.05, 0.092]],
         'trash_type': trash_types.TrashTypes.ELECTRONIC
     }
     PAPER_BOX = {
         'path': r'models/YcbCrackerBox/model.urdf',
-        'location': [-0.15, -2.5, 0.57],
-        'gripping_points': [[0.2, 0.11, 0.175]],
+        'location': [-0.2, -2.5, 0.61],
+        'gripping_points': [[-0.05, 0.05, 0.185]],
         'trash_type': trash_types.TrashTypes.PAPER
     }
