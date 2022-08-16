d81813df81022b6d9e515e7b1fcb3589b51ab94f
nirzezak <nirzezak@mail.tau.ac.il>
2022-08-16 16:36:06 +0300

    Improving configs for paperbox and mustard

--- configs/trash_configs.py
+++ configs/trash_configs.py
@@ -6,19 +6,19 @@ class TrashConfig(Enum):
 
     MUSTARD = {
         'path': r'models/YcbMustardBottle/model.urdf',
-        'location': [-0.2, -2.5, 0.61],
-        'gripping_points': [[0.02, 0.05, 0.175]],
+        'location': [-0.21, -2.5, 0.585],
+        'gripping_points': [[0.025, 0.05, 0.175]],
         'trash_type': trash_types.TrashTypes.PLASTIC
     }
     METAL_CAN = {
         'path': r'models/YcbMasterChefCan/model.urdf',
-        'location': [-0.2, -2.5, 0.61],
+        'location': [-0.2, -2.5, 0.55],
         'gripping_points': [[0.02, 0.05, 0.092]],
         'trash_type': trash_types.TrashTypes.ELECTRONIC
     }
     PAPER_BOX = {
         'path': r'models/YcbCrackerBox/model.urdf',
-        'location': [-0.21, -2.5, 0.61],
+        'location': [-0.21, -2.5, 0.6],
         'gripping_points': [[-0.04, 0.05, 0.195]],
         'trash_type': trash_types.TrashTypes.PAPER
     }
