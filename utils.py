c83a3b588e5a1f47991dbd5129213d4e4df687db
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2022-08-21 14:07:10 +0300

    Added calc_angle function to utils.py
    Will be used to calculate the angle between the arm, trash and bin.

--- utils.py
+++ utils.py
@@ -1,6 +1,20 @@
+import math
+import numpy as np
+
 def add_element_wise(l1, l2):
     """
     :param l1, l2: lists
     :returns l1 + l2 element-wise
     """
     return [sum(x) for x in zip(l1, l2)]
+
+
+def calc_angle(a, b, c):
+    """
+    @param a, b, c: 2d points
+    @returns the angle abc in radians
+    abc defines two angles, we return the angle with the lower absolute number
+    """
+    angle = math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0])
+    angle = angle % (2 * np.pi)
+    return angle if angle < np.pi else - 2 * np.pi + angle
