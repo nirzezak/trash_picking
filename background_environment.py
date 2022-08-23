8bb8b1822c3c259e2acc09b8bd56ad42c852cfe5
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2022-08-23 13:04:41 +0300

    Higher trash lifting
    Reason: wasn't higher enough for the paper box

--- background_environment.py
+++ background_environment.py
@@ -173,7 +173,7 @@ class BackgroundEnv(Environment):
         if self.is_rotation_to_the_same_point(curr_arms, rotate_degree_lst):
             # --> len(curr_arms) = 2
             # Set different y offset to avoid collision
-            vertical_offset_from_bin_lst = [0.65, 1.1]
+            vertical_offset_from_bin_lst = [0.75, 1.2]
         else:
             # The arms turn to different places, so they won't collide, we can use normal y offset
             vertical_offset_from_bin_lst = [0.85 for _ in range(len(curr_arms))]
